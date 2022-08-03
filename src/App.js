ce39b26b796c2c9ba76ef3d7d6b4643973398916
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-03 21:07:30 +0700

    Menambahkan Routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { Home, AddProductAdmin, AddTopingAdmin, Cart, DetailProduct, IncomeTransactionAdmin, Profile } from './pages'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/add-product" element={<AddProductAdmin />} />
+      <Route path="/add-toping" element={<AddTopingAdmin />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/detail-product/:id" element={<DetailProduct />} />
+      <Route path="/income-transaction" element={<IncomeTransactionAdmin />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import AddProductAdmin from "./AddProductAdmin";
+import AddTopingAdmin from "./AddTopingAdmin";
+import Cart from "./Cart";
+import DetailProduct from "./DetailProduct";
+import IncomeTransactionAdmin from "./IncomeTransactionAdmin";
+import Profile from "./Profile";
+
+export { Home, AddProductAdmin, AddTopingAdmin, Cart, DetailProduct, IncomeTransactionAdmin, Profile }
